818da80c8d54d4a760f8e9bde8a82143cda65165
tomdkd <dominik.thomasdl@gmail.com>
2026-06-14 13:32:19 +0200

    fix: front config who created error on docker image build

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -1,19 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: resolve(__dirname, '../back/public/dist'),
-    emptyOutDir: true,
-  },
   server: {
     host: true,
     port: 5173,
     proxy: {
       '/api': {
-        target: 'http://backend:8000',
+        target: 'http://php-fpm:8000',
         changeOrigin: true,
       },
     },
